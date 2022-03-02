cd9a0021d71af4a2252f655d9d908289678a1046
Beth <bethmelmtv@gmail.com>
2022-03-02 09:39:59 -0800

    added orange to buttons

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 /// grab DOM elements we want to change by id.
 const showButton = document.getElementById('btn');
 const animal = document.getElementById('animalDiv');
-
-///console.log(showButton, animal, 'validate to make sure we grabbed them')
-// set event listeners
 showButton.addEventListener('click', () => {
 ///console.log(' show button clicked', 'validate button is connected')
 /// this will remove the hidden class and display you favorite animal.       
     animal.classList.remove('hidden');
+});
+
+const showcoloranswer = document.getElementById('colorbtn');
+const color = document.getElementById('favcolor');
+showcoloranswer.addEventListener('click', () => {
+    color.classList.remove('hidden');
+});
+
+const showoccupationanswer = document.getElementById('dreamoccbtn');
+const occupationanswer = document.getElementById('occupation');
+showoccupationanswer.Button.addEventListener('click', () => {
+    occupationanswer.classList.remove('hidden');
 });
